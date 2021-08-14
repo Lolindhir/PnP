bfa6273d84510e6d7ea31ffeae9526cc913d8204
Lolindhir <88049173+Lolindhir@users.noreply.github.com>
2021-08-14 23:26:18 +0200

    Initiale Formatierung der Zauberliste abgeschlossen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
 import { MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -31,6 +32,7 @@ import { SpellFilterPipe } from '@pipes/spell-filter.pipe';
     MatFormFieldModule,
     MatInputModule,
     MatDividerModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
