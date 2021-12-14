3164d2ee66a92114e4ba32ed4ce80250075bed67
Lolindhir <88049173+Lolindhir@users.noreply.github.com>
2021-12-14 15:32:02 +0100

    Strixhaven eingeführt

--- src/shared/services/spell.service.ts
+++ src/shared/services/spell.service.ts
@@ -213,7 +213,7 @@ export class SpellService {
     sourceFilterGroupOptions.push({name: 'Official Core Books', filters: coreFilters});
 
     //official others
-    var officialFilters: SpellFilter[] = this.addFiltersByStringValue(['Official', 'Unearthed', 'Wildemount', 'Fizban'], sourceFilterOptions);
+    var officialFilters: SpellFilter[] = this.addFiltersByStringValue(['Official', 'Unearthed', 'Wildemount', 'Fizban', 'Strixhaven'], sourceFilterOptions);
     sourceFilterGroupOptions.push({name: 'Official Other Content', filters: officialFilters});
 
     //third-party (all others)
