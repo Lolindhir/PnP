15d2f3df607bdb3dd0c43c31a421623f92a82353
Lolindhir <88049173+Lolindhir@users.noreply.github.com>
2024-07-07 22:46:25 +0200

    Home-Navigation explizit eingeführt und auf Startseite Doppelnavigation entfernt

--- src/app/main-header/main-header.component.ts
+++ src/app/main-header/main-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-header',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ){ }
 
   ngOnInit(): void {
+  }
+
+  isCurrentRouteHome(): boolean {
+    if (this.router.url.includes('/home')) {
+      return true;
+    }
+    return false;
   }
 
 }
